f6a7feadaf7549aa529f6762e19fd94491f9fa9d
Celinejoo <87295179+Celinejoo@users.noreply.github.com>
2022-10-31 23:07:40 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -40,5 +40,43 @@ $(function(){
 
     })
 
+    $("#slider").slider({
+        range: true,
+        min: 5,
+        max: 30,
+        step: 5,
+        values: [100, 500],
+        slide: function(e, ui) {
+            var delay = function() {
+                var handleIndex = $(ui.handle).data('index.uiSliderHandle');
+                var label = handleIndex == 0 ? '#min' : '#max';
+                $(label).html('$' + ui.value).position({
+                    my: 'center top',
+                    at: 'center bottom',
+                    of: ui.handle,
+                    offset: "0, 10"
+                });
+            };
+            
+            // wait for the ui.handle to set its position
+            setTimeout(delay, 5);
+        }
+    });
+    
+    $('#min').html('$' + $('#slider').slider('values', 0)).position({
+        my: 'center top',
+        at: 'center bottom',
+        of: $('#slider a:eq(0)'),
+        offset: "0, 10"
+    });
+    
+    $('#max').html('$' + $('#slider').slider('values', 1)).position({
+        my: 'center top',
+        at: 'center bottom',
+        of: $('#slider a:eq(1)'),
+        offset: "0, 10"
+
+    });
+
     
 })
